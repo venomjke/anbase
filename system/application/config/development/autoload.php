236e315a0485d2c26feb05dd8e96aeea50391a33
alex.strigin <apstrigin@gmail.com>
2012-03-19 00:21:08 +0400

    Set up migrations for application that include db settings

--- system/application/config/development/autoload.php
+++ system/application/config/development/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('session','form_validation','template');
+$autoload['libraries'] = array('session','form_validation','template','database');
 
 
 /*
