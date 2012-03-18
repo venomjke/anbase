36f5e473855afee530ce3f397fef9f4f7e638973
alex.strigin <apstrigin@gmail.com>
2012-03-19 00:56:01 +0400

    added translation of migration library into russian to the framework

---
+++ system/framework/language/russian/migration_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['migration_none_found']			= "Ни одной миграции не было найдено.";
+$lang['migration_not_found']			= "Выбранная миграция не найдена.";
+$lang['migration_multiple_version']		= "Эти миграции имеют один и тот-же номер: %d.";
+$lang['migration_class_doesnt_exist']	= "Класс миграции \"%s\" не может быть найден.";
+$lang['migration_missing_up_method']	= "Класс миграции \"%s\" потерял 'up' метод.";
+$lang['migration_missing_down_method']	= "Класс миграции \"%s\" потерял 'down' метод.";
+$lang['migration_invalid_filename']		= "Миграция \"%s\" имеет не верное имя файла.";
+
+
+/* End of file migration_lang.php */
+/* Location: ./system/language/english/migration_lang.php */
