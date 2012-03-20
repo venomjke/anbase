afecab4c2d87a26b53e744f28124293220b9dabc
alex.strigin <apstrigin@gmail.com>
2012-03-20 22:21:00 +0400

    base layout structure

--- themes/start/views/layouts/default.php
+++ themes/start/views/layouts/default.php
@@ -6,6 +6,38 @@
 		?>
 	</head>
 	<body>
-		<?php echo $template['body']; ?>
+		<div id="head">
+			<ul id="menu">
+				<li>
+					<a href="#"> Демо | </a>
+				</li>
+				<li>
+					<a href="#"> Регистрация | </a>
+				</li>
+				<li>
+					<a href="#"> О системе | </a>
+				</li>
+				<li>
+					<a href="#"> Цены |</a>
+				</li>
+				<li>
+					<a href="#"> О нас | </a>
+				</li>
+				<li>
+					<a href="#"> FAQ </a>
+				</li>
+			</ul>
+		</div>
+		<div id="content">
+			<?php echo $template['body']; ?>
+		</div>
+		<div id="footer">
+			<div id="stats">
+				Время выполнения: 	<?php echo $this->benchmark->elapsed_time(); ?>
+			</div>
+			<div id="copyright">
+				(c) copyright 2012 Flyweb inc.
+			</div>
+		</div>
 	</body>
 </html>
