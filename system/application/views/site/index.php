f93e47d8987606a1b318358ce211f854840b1dd5
alex.strigin <apstrigin@gmail.com>
2012-03-21 02:05:36 +0400

    created base loginBox

--- system/application/views/site/index.php
+++ system/application/views/site/index.php
@@ -1,9 +1,33 @@
-<h1> Index page flyweb anbase project! </h1>
+<div class="contentWrap">
 
-
-<?php echo form_open("site/index"); ?>
-<?php echo form_hidden("test_field","blablabla"); ?>
-<?php echo form_submit("submit",'my submit!'); ?>
-<?php echo form_close(); ?>
-
-<?php echo "Execution time is - ".$this->benchmark->elapsed_time(); ?>
+	<div id="blog">
+		<h2>Новости ( Блог )</h2>
+		<div class="blogPost">
+			Пост какой-то
+		</div> 
+		<div class="blogPost">
+			Пост какой-то
+		</div>
+    </div>
+	<div id="loginBox">
+		<h3> Вход в панель </h3>
+		<div id="loginWrap">
+			<?php echo form_open("login"); ?>
+			<div class="loginField">
+				<h4> Логин </h4>
+				<?php echo form_input("login","",'placeholder="Логин"'); ?>
+			</div>
+			<div class="loginField">
+				<h4> Пароль </h4>
+				<?php echo form_password("password","",'placeholder="******"'); ?>
+			</div>
+			<div class="loginField">
+				<?php echo form_checkbox("remember","1"); ?> Запомнить | <?php echo anchor("forget_password","Забыли пароль?"); ?>
+			</div>
+			<div class="loginField">
+				<?php echo form_submit("Submit","Войти"); ?>
+			</div>
+			<?php echo form_close(); ?>
+		</div>
+	</div>
+</div>
