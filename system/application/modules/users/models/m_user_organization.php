53ef588f9d2c7c943c4863663bc200ed1b60ce02
alex.strigin <apstrigin@gmail.com>
2012-03-23 17:52:54 +0400

    add method that get info about user and org once

--- system/application/modules/users/models/m_user_organization.php
+++ system/application/modules/users/models/m_user_organization.php
@@ -34,4 +34,29 @@ class M_User_organization extends MY_Model{
 		$this->validate = array();
 
 	}
+
+	/**
+	 * Метод выбирает инфу о юзере из таблицы users_organizations присоединяя также инфу
+	 * из users и orgs, а именно из users fio,role,phone а из orgs ceo
+	 * 
+	 * @param int id
+	 * @return void
+	 * @author 
+	 **/
+	public function get_full_user_info($id)
+	{
+		$this->select('
+			users_organizations.org_id,
+			users_organizations.user_id,
+			users.name,
+			users.last_name,
+			users.middle_name,
+			users.role,
+			users.phone,
+			organizations.ceo
+		')->join('users','users.id = users_organizations.user_id')
+		  ->join('organizations','users_organizations.org_id = organizations.id');
+
+		return $this->get(array('users_organizations.user_id' => $id));
+	}
 }
