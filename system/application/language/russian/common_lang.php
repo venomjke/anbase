dd5c3b5a1315f4c1479d49c8de1d8fefe3eb2cb5
alex.strigin <apstrigin@gmail.com>
2012-03-27 20:57:18 +0400

    created file for common russian messages

---
+++ system/application/language/russian/common_lang.php
@@ -0,0 +1,10 @@
+<?php defined("BASEPATH") or die("No direct access to script");
+
+
+/*
+*
+* Ошибки БД и валидации
+*
+*/
+$lang['common.not_recive_data'] = 'Данные не получены';
+$lang['common.update_error']    = 'Не удалось выполнить обновление записи';
