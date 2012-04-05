64d277ad2b10c481b5d5ed9620ee7bd8f32477c3
alex.strigin <apstrigin@gmail.com>
2012-04-05 15:08:42 +0400

    del old m_invite_user update new

--- system/application/models/m_invite_user.php
+++ system/application/models/m_invite_user.php
@@ -33,7 +33,7 @@ class M_Invite_user extends MY_Model
 		* Правила валидации
 		*/
 		$this->validate = array(
-			array('field'=>'email', 'label'=>'lang:label_email', 'rules'=>'xss_clean|valid_email'),
+			array('field'=>'email', 'label'=>'lang:label_email', 'rules'=>'xss_clean|valid_email|max_length[100]'),
 			array('field'=>'manager_id', 'label'=>'Manager Id', 'rules'=>'is_natural')
 		);
 	}
@@ -47,5 +47,40 @@ class M_Invite_user extends MY_Model
 	public function get_validation_rules()
 	{
 		return $this->validate;
+	}
+
+	/**
+	 * Проверка, существует ли инвайт с заданными key_id и email
+	 *
+	 * @return boolean
+	 * @author alex.strigin
+	 **/
+	public function is_exists_invite($key_id,$email,$role)
+	{
+		return $this->count_all_results(array('key_id'=>$key_id,'email'=>$email,'role'=>$role)) == 0?false:true;
+	}
+
+
+	/**
+	 * Загрузка инвайта
+	 *
+	 * @return object
+	 * @author alex.strigin
+	 **/
+	public function load_invite($key,$email)
+	{
+		$this->select($this->table.".*");
+		$this->select("organizations.name as org_name");
+		$this->select("organizations.phone as org_phone");
+		$this->select("users.name as ceo_name");
+		$this->select("users.middle_name as ceo_middle_name");
+		$this->select("users.last_name as ceo_last_name");
+		$this->from($this->table);
+		$this->join("organizations","organizations.id = invites_users.org_id")
+		     ->join("users","users.id = organizations.ceo");
+		$this->where($this->table.".key_id",$key);
+		$this->where($this->table.".email",$email);
+		$this->limit(1);
+		return $this->db->get()->row();
 	}
 } // END class M_Invite_user extends MY_Model
