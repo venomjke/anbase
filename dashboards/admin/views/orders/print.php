0a69f58c813408b858cce509dd2a766e4328d929
alex.strigin <apstrigin@gmail.com>
2012-05-11 19:57:46 +0400

    update print_table style

--- dashboards/admin/views/orders/print.php
+++ dashboards/admin/views/orders/print.php
@@ -1,7 +1,7 @@
 <?php
 	$orders_table = array();
 	$orders_table_head = array('Номер','Описание','Цена','Телефон');
-	$orders_table_tmpl = array('table_open'=>'<table border="1" cellpadding="2" style="width:100%">');
+	$orders_table_tmpl = array('table_open'=>'<table border="0" class="print_table" cellspacing="0" cellpadding="2" style="width:95%">');
 	$this->table->set_heading($orders_table_head);
 	$this->table->set_template($orders_table_tmpl);
 
