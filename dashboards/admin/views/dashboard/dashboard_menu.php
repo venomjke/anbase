27a44040c41077a110d73960498b5572937c9f20
alex.strigin <apstrigin@gmail.com>
2012-05-15 21:47:55 +0400

    rm settings item from admin dashboard

--- dashboards/admin/views/dashboard/dashboard_menu.php
+++ dashboards/admin/views/dashboard/dashboard_menu.php
@@ -3,7 +3,6 @@
 
     <ul>
 	    <li class="item1"><a href="<?php echo site_url("logout");?>">Выход</a></li>
-	    <li class="item5"><a href="<?php echo site_url("admin/settings");?>">Настройки</a></li>
 	    <li class="item2"><a href="<?php echo site_url("admin/profile");?>">Профиль</a></li>
 	    <li class="item3"><a href="<?php echo site_url("admin/user");?>">Пользователи</a></li>
 	    <li class="item4"><a href="<?php echo site_url("admin/orders");?>">Заявки</a></li>
