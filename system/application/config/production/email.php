40b7c398f9379bfdec0aa25c055c98add4f35eff
alex.strigin <apstrigin@gmail.com>
2012-05-23 01:38:08 +0400

    update production configs

--- system/application/config/production/email.php
+++ system/application/config/production/email.php
@@ -7,14 +7,19 @@
 * 
 *
 */
-$config['protocol'] = 'mail';
-
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'ssl://smtp.googlemail.com';
+$config['smtp_port'] = 465;
+$config['smtp_user'] = 'no-reply@anbase.ru';
+$config['smtp_pass'] = 'NO_REPLY()(%';
+$config['mailtype']  = 'html';
 $config['useragent'] = 'bot';
+
 
 /*
 * mailpath for protocol sendmail
 */
 //$config['mailpath']
 
-$config['mailtype'] = 'html';
-$config['charset']  = 'utf-8';
+//$config['mailtype'] = 'html';
+//$config['charset']  = 'utf-8';
