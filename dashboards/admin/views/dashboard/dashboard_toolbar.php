05399fa423ba7b1f7c0fa8e48e065c00d3bd44b3
alex.strigin <apstrigin@gmail.com>
2012-05-23 02:21:33 +0400

    fix dashboard_toolbar css

--- dashboards/admin/views/dashboard/dashboard_toolbar.php
+++ dashboards/admin/views/dashboard/dashboard_toolbar.php
@@ -1,12 +1,12 @@
 <div id="dashboard_toolbar">
 
     <?php if($current != 'off'): ?>
-    <span class="plus" id="add_order" style="margin-top:5px; margin-right:16px"><a href="#" onclick="return false;">Добавить</a></span>
-    <span class="plus minus" id="finish_order" style="margin-top:5px;  margin-right:16px"><a href="#" onclick="return false;">Завершить</a></span>
+    <span class="plusplus" id="add_order" style="margin-top:5px; margin-right:16px"><a href="#" onclick="return false;">Добавить</a></span>
+    <span class="plusplus minus" id="finish_order" style="margin-top:5px;  margin-right:16px"><a href="#" onclick="return false;">Завершить</a></span>
     <?php else: ?>
-    <span class="plus" id="restore_order" style="margin-top:5px;  margin-right:16px"><a href="#" onclick="return false;">Возобновить</a></span>
+    <span class="plusplus" id="restore_order" style="margin-top:5px;  margin-right:16px"><a href="#" onclick="return false;">Возобновить</a></span>
     <?php endif; ?>
-    <span class="plus minus" id="del_order" style="margin-top:5px;  margin-right:16px"><a href="#" onclick="return false;">Удалить</a></span>
-    <span class="plus" id="print_order" style="margin-top:5px;  margin-right:16px;float:right"> <a href="#" onclick="return false;">Распечатать</a></span>
+    <span class="plusplus minus" id="del_order" style="margin-top:5px;  margin-right:16px"><a href="#" onclick="return false;">Удалить</a></span>
+    <span class="plusplus" id="print_order" style="margin-top:5px;  margin-right:16px;float:right"> <a href="#" onclick="return false;">Распечатать</a></span>
 
 </div>
